309e6956ff42ede751ccd802ee393985896c5c63
Jean-Baptiste Barth <jeanbaptiste.barth@gmail.com>
2014-11-16 12:49:31 +0100

    Move functions to utils.sh and add some comments

--- utils.sh
+++ utils.sh
@@ -1,4 +1,9 @@
-step() { echo -ne $'\e[32m* '; echo $*; echo -ne $'\e[0m'; }
+#display a main step
+step() {
+  echo -ne $'\e[32m* '; echo $*; echo -ne $'\e[0m'
+}
+
+#open a URL in the browser depending on what's available
 open_url() {
   if [[ "$OSTYPE" == "darwin"* ]]; then
     sleep 0.1; open "$1" #the "sleep" avoids weird errors in OSX Yosemite
@@ -9,4 +14,18 @@ open_url() {
   else
     echo "Unable to find a way to open URL $1 :(" >&2
   fi
+}
+
+#spawn a docker ES node
+spawn_es_node() {
+  node_name=$1
+  node_type=$2
+  port_9200=$3
+  port_9300=$4
+  docker run -d \
+    --name $node_name \
+    --publish $port_9200:9200 \
+    --publish $port_9300:9300 \
+    $(whoami)/elasticsearch-full \
+    /elasticsearch/bin/elasticsearch -Xms64m -Xmx64m -Des.config=/elasticsearch/config/elasticsearch-$node_type.yml
 }
